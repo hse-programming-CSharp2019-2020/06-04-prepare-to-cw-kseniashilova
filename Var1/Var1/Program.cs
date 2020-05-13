1ad40c3f7d94052fd1af6ce544da3e1a3838d03c
Ksenia <kashilova@edu.hse.ru>
2020-05-13 17:05:12 +0300

    hello comm1

---
+++ Var1/Var1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Var1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
